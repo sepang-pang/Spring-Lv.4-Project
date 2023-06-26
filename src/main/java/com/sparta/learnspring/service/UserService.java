fbb7a3987a6bfa79224a78a1aaac900e2d05ee3c
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-06-26 18:16:07 +0900

    <feat> : JWT 이용 로그인 구현

--- src/main/java/com/sparta/learnspring/service/UserService.java
+++ src/main/java/com/sparta/learnspring/service/UserService.java
@@ -1,10 +1,13 @@
 package com.sparta.learnspring.service;
 
 
+import com.sparta.learnspring.dto.LoginRequestDto;
 import com.sparta.learnspring.dto.SignupRequestDto;
 import com.sparta.learnspring.entity.User;
 import com.sparta.learnspring.entity.UserRoleEnum;
+import com.sparta.learnspring.jwt.JwtUtil;
 import com.sparta.learnspring.repoistory.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,10 +18,12 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
     }
 
     // ADMIN_TOKEN
@@ -53,5 +58,24 @@ public class UserService {
         // 사용자 등록
         User user = new User(username, password, email, role);
         userRepository.save(user);
+    }
+
+    public void login(LoginRequestDto requestDto, HttpServletResponse res) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다")
+        );
+
+        // 비밀번호 확인
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
+        }
+
+        // JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        String token = jwtUtil.createToken(user.getUsername(), user.getRole());
+        jwtUtil.addJwtToCookie(token, res);
     }
 }
