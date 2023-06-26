a115254a8c6de4f07da5ea5cf4fc5806cbdbe8c5
오세창 <114675677+sepang-pang@users.noreply.github.com>
2023-06-27 01:54:04 +0900

    <feat> : Validation 설정 및 예외처리 구현

--- src/main/java/com/sparta/learnspring/controller/UserController.java
+++ src/main/java/com/sparta/learnspring/controller/UserController.java
@@ -2,11 +2,18 @@ package com.sparta.learnspring.controller;
 
 import com.sparta.learnspring.dto.SignupRequestDto;
 import com.sparta.learnspring.service.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api")
+@Slf4j(topic = "UserController 로그")
 public class UserController {
 
     private final UserService userService;
@@ -34,7 +41,16 @@ public class UserController {
     }
 
     @PostMapping("user/signup")
-    public String signup(@RequestBody SignupRequestDto requestDto) {
+    public String signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
         return "redirect:/api/user/login-page";
     }
